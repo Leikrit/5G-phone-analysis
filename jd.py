900bcf7ef667d57f7cfb0e8534e1f8aed8de58be
ruiyo <162017673+ruiyo@users.noreply.github.com>
2024-04-24 20:43:21 +0800

    Add files via upload

--- jd.py
+++ jd.py
@@ -1,57 +1,3 @@
-# from lxml import etree
-# import requests
-# import time
-# import openpyxl
-#
-# outwb = openpyxl.Workbook()
-# outws = outwb.create_sheet(index=0)
-#
-# outws.cell(row=1, column=1, value="index")
-# outws.cell(row=1, column=2, value="title")
-# outws.cell(row=1, column=3, value="price")
-# outws.cell(row=1, column=4, value="id")
-#
-# def jd_page():
-#     page=1
-#     s = 1
-#     count = 1
-#     for i in range(1,6):
-#         print("page="+str(page)+",s="+str(s))
-#         url = "https://search.jd.com/search?keyword=笔记本&wq=笔记本&ev=exbrand_联想%5E&page="+str(page)+"&s="+str(s)+"&click=1"
-#         page = page+1
-#         s = s+30
-#
-#         headers = {
-#             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0",
-#             "Cookie": "jsavif=1; shshshfpa=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; shshshfpx=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; __jdv=181111935|direct|-|none|-|1712498482836; __jdu=1712498482836958502465; areaId=19; thor=676741B8CA3FCE6E194F2D84F06C5CB3344018AEFA0977C08C33E05EF2AEFFC2E1A76BBB8EAEF54F167E0D5A091521C6C2647357A39A1425135A7B5D264B672461AA9B3FECC7E09DCA53EA4E31691ED9A536B6BBEC8FCD36A2255318F5481F71FAE3BDB5358DC3CB614F81F015A4A5493AFF45191AEBE70515C825B934B0DE5527DC7434634488AF35A89190257A5DB45A80FB5111C9A05CCD4F62CBD799AA35; flash=2_XCzzsYZ0FyMqOJoQdXzMtNQSvvqXN4q-SESB_0zOuXCIW6UYO6hBlJTCckeR68kqg0lk341UEqLcxbs8PCNHXTrPHmOUxb3pnoRtfkq1QpL*; pinId=BKmhqjK0Ot4FvWOdtn9TiA; pin=jd_lTmXtaLBvUsY; unick=jd_lTmXtaLBvUsY; ceshi3.com=000; _tp=7%2B9vkUgexJVrllJMczJ5lw%3D%3D; _pst=jd_lTmXtaLBvUsY; token=823960cc5f2c76f69f3dc03669daf4ec,3,951388; __tk=20128a4f95f573c29e9f0fc5f12745d2,3,951388; ipLoc-djd=19-1601-50258-129167; __jda=143920055.1712498482836958502465.1712498483.1712498483.1712498483.1; __jdc=143920055; user-key=d4e0b5c4-efd8-4e69-87e6-a0f85f57fe3a; cn=0; __jdb=143920055.11.1712498482836958502465|1.1712498483; 3AB9D23F7A4B3CSS=jdd032ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQAAAAMOXDZFCMQAAAAADWECUZTLHN6M6UX; _gia_d=1; shshshfpb=BApXeDsL6u-tAmC3SbfNMBEgpx2ei76hCBlAIg61t9xJ1MrE1XoO2; 3AB9D23F7A4B3C9B=2ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQ"
-#         }
-#         res = requests.get(url, headers=headers)
-#         # time.sleep(2)
-#         res.encoding = 'utf-8'
-#         text = res.text
-#
-#         selector = etree.HTML(text)
-#         list = selector.xpath('//*[@id="J_goodsList"]/ul/li')
-#
-#         for i in list:
-#             title = i.xpath('.//div[@class="p-name p-name-type-2"]/a/em/text()')[0]
-#             price = i.xpath('.//div[@class="p-price"]/strong/i/text()')[0]
-#             product_id = i.xpath('.//div[@class="p-commit"]/strong/a/@id')[0].replace("J_comment_", "")
-#             print("title " + str(title))
-#             print("price= " + str(price))
-#             print("product_id= " + str(product_id))
-#             print("-----")
-#             count += 1
-#             outws.cell(row=count, column=1, value=str(count - 1))
-#             outws.cell(row=count, column=2, value=str(title))
-#             outws.cell(row=count, column=3, value=str(price))
-#             outws.cell(row=count, column=4, value=str(product_id))
-#
-#
-# jd_page()
-# outwb.save("xx.xls")  #保存
-
-
 from lxml import etree
 import requests
 import time
@@ -62,14 +8,10 @@ outws = outwb.create_sheet(index=0)
 
 outws.cell(row=1, column=1, value="index")
 outws.cell(row=1, column=2, value="id")
-outws.cell(row=1, column=3, value="title")
+outws.cell(row=1, column=3, value="name")
 outws.cell(row=1, column=4, value="price")
 
 def jd_page():
-    #  https://search.jd.com/Search?keyword=5g%E6%89%8B%E6%9C%BA&suggest=1.his.0.0&wq=5g%E6%89%8B%E6%9C%BA&pvid=feb1e81188ca4a11964991bce3bbf228&isList=0&page=1&s=1&click=0
-    #  https://search.jd.com/Search?keyword=5g%E6%89%8B%E6%9C%BA&suggest=1.his.0.0&wq=5g%E6%89%8B%E6%9C%BA&pvid=feb1e81188ca4a11964991bce3bbf228&isList=0&page=3&s=56&click=0
-    #  https://search.jd.com/Search?keyword=5g%E6%89%8B%E6%9C%BA&suggest=1.his.0.0&wq=5g%E6%89%8B%E6%9C%BA&pvid=feb1e81188ca4a11964991bce3bbf228&isList=0&page=5&s=116&click=0
-    #  https://search.jd.com/Search?keyword=5g%E6%89%8B%E6%9C%BA&suggest=1.his.0.0&wq=5g%E6%89%8B%E6%9C%BA&pvid=feb1e81188ca4a11964991bce3bbf228&isList=0&page=7&s=176&click=0
     page=1
     s = 1
     count = 1
@@ -81,10 +23,10 @@ def jd_page():
 
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0",
-            "Cookie": "jsavif=1; shshshfpa=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; shshshfpx=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; __jdv=181111935|direct|-|none|-|1712498482836; __jdu=1712498482836958502465; areaId=19; thor=676741B8CA3FCE6E194F2D84F06C5CB3344018AEFA0977C08C33E05EF2AEFFC2E1A76BBB8EAEF54F167E0D5A091521C6C2647357A39A1425135A7B5D264B672461AA9B3FECC7E09DCA53EA4E31691ED9A536B6BBEC8FCD36A2255318F5481F71FAE3BDB5358DC3CB614F81F015A4A5493AFF45191AEBE70515C825B934B0DE5527DC7434634488AF35A89190257A5DB45A80FB5111C9A05CCD4F62CBD799AA35; flash=2_XCzzsYZ0FyMqOJoQdXzMtNQSvvqXN4q-SESB_0zOuXCIW6UYO6hBlJTCckeR68kqg0lk341UEqLcxbs8PCNHXTrPHmOUxb3pnoRtfkq1QpL*; pinId=BKmhqjK0Ot4FvWOdtn9TiA; pin=jd_lTmXtaLBvUsY; unick=jd_lTmXtaLBvUsY; ceshi3.com=000; _tp=7%2B9vkUgexJVrllJMczJ5lw%3D%3D; _pst=jd_lTmXtaLBvUsY; token=823960cc5f2c76f69f3dc03669daf4ec,3,951388; __tk=20128a4f95f573c29e9f0fc5f12745d2,3,951388; ipLoc-djd=19-1601-50258-129167; __jda=143920055.1712498482836958502465.1712498483.1712498483.1712498483.1; __jdc=143920055; user-key=d4e0b5c4-efd8-4e69-87e6-a0f85f57fe3a; cn=0; __jdb=143920055.11.1712498482836958502465|1.1712498483; 3AB9D23F7A4B3CSS=jdd032ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQAAAAMOXDZFCMQAAAAADWECUZTLHN6M6UX; _gia_d=1; shshshfpb=BApXeDsL6u-tAmC3SbfNMBEgpx2ei76hCBlAIg61t9xJ1MrE1XoO2; 3AB9D23F7A4B3C9B=2ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQ"
+            "Cookie": "shshshfpa=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; shshshfpx=076e83c2-9fb4-a2b0-fae0-ae1019291998-1712498482; __jdu=1712498482836958502465; pinId=BKmhqjK0Ot4FvWOdtn9TiA; pin=jd_lTmXtaLBvUsY; unick=jd_lTmXtaLBvUsY; _tp=7%2B9vkUgexJVrllJMczJ5lw%3D%3D; _pst=jd_lTmXtaLBvUsY; user-key=d4e0b5c4-efd8-4e69-87e6-a0f85f57fe3a; qrsc=3; unpl=JF8EALFnNSttWk1dAxsHThtHQ1oHW1gATEQBamQFAAlcTl1QSAodQUV7XlVdWBRKEh9vYRRVVVNKXA4aACsSEXteU11bD00VB2xXXAQDGhUQR09SWEBJJVlQXl4ITxcFZ2A1ZF5Ye1QEKwIdEhNOVV1bVAtCFQRnYwxQXFtOUwErAysVIHttU1ZYDUoQM25XBGQfDBdWAR0HGRtdS1tUXVgAQhIKbG4HU1VcQlAEGAccFiBKbVc; __jdv=76161171|haosou-search|t_262767352_haosousearch|cpc|5512151796_0_378714d8f96b4496b3421ee548db97bd|1713316144511; areaId=19; ipLoc-djd=19-1601-50258-129167; TrackID=1JSb7ijeU0EfdV5-iYXc4__5LdRAVrkfc97Iias3TkhIZsyRi6cA5FBrkNtYGRbP1WtiIvD7yahJxauK6S0JYVG9it7lQFlrvRA4QzTFYeSfUyEO84P7E9vWFMUezQeny; thor=676741B8CA3FCE6E194F2D84F06C5CB3344018AEFA0977C08C33E05EF2AEFFC2D0A42EA46D88CF2D6B178FDAAA985F63FAA583759B36AB62234AB1B33A45A455ACF514C7394AA90420946DB02EBFC9C9086C3CF93A55E3FBDCCB979E1BD583B396581BA939E938169295926E7780CF82164F7B763197BB10E8D1B3CC51D2C0BD2D5583BD01464691741F806AECA53B8CB703008F72C1F3F11C1FDB7DC4C60A0A; flash=2_Ts9f6zFn2Tqw0NqMxG4hqLTQ13GCG1ID25q2whi2C-wINYjIm1mJlK882w-DJEDo53i-11Mh_olckTVhMFBu14BGBMXX3jll3hQvglus2d5*; jsavif=1; jsavif=1; _gia_d=1; 3AB9D23F7A4B3CSS=jdd032ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQAAAAMPBW7FEQAAAAAACFT2A57E3VCIIUX; shshshfpb=BApXckia2DupAmC3SbfNMBEgpx2ei76hCBlAIg6169xJ1MrE1XoO2; avif=1; xapieid=jdd032ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQAAAAMPBW7FEQAAAAAACFT2A57E3VCIIUX; __jda=143920055.1712498482836958502465.1712498483.1713749255.1713922086.10; __jdb=143920055.5.1712498482836958502465|10.1713922086; __jdc=143920055; rkv=1.0; 3AB9D23F7A4B3C9B=2ZJOWCZ7WY5TLDNLMCMU4UZPUPCMOTWAXIN3TSF6B3HTD5DRF55DEGHKITFZ6WJG6UTQJRXS2KAGJ4MUOIRCBZU3GQ"
         }
         res = requests.get(url, headers=headers)
-        # time.sleep(2)
+        time.sleep(1.5)
         res.encoding = 'utf-8'
         text = res.text
 
@@ -92,19 +34,19 @@ def jd_page():
         list = selector.xpath('//*[@id="J_goodsList"]/ul/li')
 
         for i in list:
-            title = i.xpath('.//div[@class="p-name p-name-type-2"]/a/em/text()')[0]
-            price = i.xpath('.//div[@class="p-price"]/strong/i/text()')[0]
+            product_name = i.xpath('.//div[@class="p-name p-name-type-2"]/a/em/text()')[0]
+            product_price = i.xpath('.//div[@class="p-price"]/strong/i/text()')[0]
             product_id = i.xpath('.//div[@class="p-commit"]/strong/a/@id')[0].replace("J_comment_", "")
-            print("title " + str(title))
-            print("price= " + str(price))
             print("product_id= " + str(product_id))
+            print("product_name= " + str(product_name))
+            print("product_price= " + str(product_price))
             print("-----")
             count += 1
             outws.cell(row=count, column=1, value=str(count - 1))
-            outws.cell(row=count, column=2, value=str(title))
-            outws.cell(row=count, column=3, value=str(price))
-            outws.cell(row=count, column=4, value=str(product_id))
+            outws.cell(row=count, column=2, value=str(product_id))
+            outws.cell(row=count, column=3, value=str(product_name))
+            outws.cell(row=count, column=4, value=str(product_price))
 
 
 jd_page()
-outwb.save("5G_phone.xls")  #保存
+outwb.save("jd.xlsx")  #保存
--- jd_comment.py
+++ jd_comment.py
@@ -5,13 +5,12 @@ import random
 import xlwt
 import xlutils.copy
 import xlrd
+import openpyxl
 
-def start(page):
+def start(page, score, id):
     # 获取URL
     #score 评价等级 page=0 第一页 producitid 商品类别
-    url = 'https://club.jd.com/comment/productPageComments.action?&productId=100031192620&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1'
-
-    # url = 'https://club.jd.com/comment/productPageComments.action?&productId=100016647456&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1'
+    url = 'https://club.jd.com/comment/productPageComments.action?&productId=' + str(id) + '&score=' + str(score) + '&sortType=5&page='+ str(page) + '&pageSize=10&isShadowSku=0&fold=1'
     headers= {
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36"
     }
@@ -25,26 +24,26 @@ def parse(data):
     items = data['comments']
     for i in items:
         yield (
-            i['nickname'],#用户名自
+            i['nickname'],#用户名字
             i['id'], #用户id
             i['content'],#内容
             i['creationTime']#时间
         )
 
-def excel(items):
+def excel(items, id):
     #第一次写入
-    newTable = "test.xls"#创建文件
+    newTable = str(id) + ".xlsx"#创建文件
     wb = xlwt.Workbook("encoding='utf-8")
 
     ws = wb.add_sheet('sheet1')#创建表
-    headDate = ['id', '内容', '时间']#定义标题
-    for i in range(0,3):#for循环遍历写入
+    headDate = ['用户名','id', '内容', '时间']#定义标题
+    for i in range(0,4):#for循环遍历写入
         ws.write(0, i, headDate[i], xlwt.easyxf('font: bold on'))
 
     index = 1#行数
 
     for data in items:#items是十条数据 data是其中一条（一条下有三个内容）
-        for i in range(0,3):#列数
+        for i in range(0,4):#列数
 
             print(data[i])
             ws.write(index, i, data[i])#行 列 数据（一条一条自己写入）
@@ -52,25 +51,25 @@ def excel(items):
         index += 1#等上一行写完了 在继续追加行数
         wb.save(newTable)
 
-def another(items, j):#如果不是第一次写入 以后的就是追加数据了 需要另一个函数
+def another(items, j, id):#如果不是第一次写入 以后的就是追加数据了 需要另一个函数
 
     index = (j-1) * 10 + 1#这里是 每次写入都从11 21 31..等开始 所以我才传入数据 代表着从哪里开始写入
 
-    data = xlrd.open_workbook('test.xls')
+    data = xlrd.open_workbook(str(id) + ".xlsx")
     ws = xlutils.copy.copy(data)
     # 进入表
     table = ws.get_sheet(0)
 
     for test in items:
 
-        for i in range(0, 3):#跟excel同理
+        for i in range(0, 4):#跟excel同理
             print(test[i])
 
             table.write(index, i, test[i])  # 只要分配好 自己塞入
         print('_______________________')
 
         index += 1
-        ws.save('test.xls')
+        ws.save(str(id) + ".xlsx")
 
 
 
@@ -78,19 +77,26 @@ def main():
     j = 1#页面数
     judge = True#判断写入是否为第一次写入
 
-    for i in range(0, 100):
-        time.sleep(1.5)
-        #记得time反爬 其实我在爬取的时候没有使用代理ip也没给我封 不过就当这是个习惯吧
-        first = start(j)
-        test = parse(first)
+    wb = openpyxl.load_workbook(filename="jd.xlsx")
+    ws = wb['Sheet1']
+    for row in ws.iter_rows(min_row=2, min_col=2, max_col=2, max_row=151, values_only=True):
+        print(row[0])
+        for i in range(0, 10):
+            for k in range(0, 5):
+                time.sleep(1.5)
+                # 记得time反爬 其实我在爬取的时候没有使用代理ip也没给我封 不过就当这是个习惯吧
+                first = start(j, k, row[0])
+                test = parse(first)
 
-        if judge:
-            excel(test)
-            judge = False
-        else:
-            another(test, j)
-        print('第' + str(j) + '页抓取完毕\n')
-        j = j + 1
+                if judge:
+                    excel(test, row[0])
+                    judge = False
+                else:
+                    another(test, j, row[0])
+                print('第' + str(j) + '页抓取完毕\n')
+                j = j + 1
+        break
+
 
 
 if __name__ == '__main__':
